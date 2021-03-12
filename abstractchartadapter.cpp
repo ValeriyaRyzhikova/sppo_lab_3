7b0ea4e2fff4cc7d21bc8652aa4f424f2434ec4b
ValeriaRizhikova <rvaleriav@yandex.ru>
2021-03-12 13:31:25 +0300

    added abstract chart adapter

---
+++ abstractchartadapter.cpp
@@ -0,0 +1,43 @@
+#include "abstractchartadapter.h"
+
+AbstractChartAdapter::AbstractChartAdapter()
+{
+    view = new QtCharts::QChartView();
+    model = new QtCharts::QChart();
+    view->setRenderHint(QPainter::Antialiasing);
+    view->setChart(model);
+    showLegend();
+}
+
+AbstractChartAdapter::~AbstractChartAdapter()
+{
+    delete view;
+    delete model;
+}
+
+void AbstractChartAdapter::updateAdapter(QMap<QString,float> m)
+{
+    model->removeAllSeries();
+    model->addSeries(putDataInSeries(makeMapToVector(m)));
+    model->setAnimationOptions(QtCharts::QChart::SeriesAnimations);
+    emit sentMainwindow(view);
+}
+
+void AbstractChartAdapter::showLegend()
+{
+    model->legend()->setVisible(true);
+    model->legend()->setAlignment(Qt::AlignRight);
+}
+
+QVector<QPair<QString,float>> AbstractChartAdapter::makeMapToVector(const QMap<QString, float> &m)
+{
+    QVector<QPair<QString,float>> result;
+    QMapIterator<QString, float> iter(m);
+    while (iter.hasNext()) {
+        iter.next();
+        result.push_back(QPair(iter.key(),iter.value()));
+    }
+    return result;
+}
+
+
---
+++ abstractchartadapter.h
@@ -0,0 +1,29 @@
+#ifndef ABSTRACTCHARTADAPTER_H
+#define ABSTRACTCHARTADAPTER_H
+#include<QtCharts/QChart>
+#include <QtCharts/QBarSeries>
+#include <QtCharts/QChartView>
+#include <QtCharts/QValueAxis>
+#include <QtCharts/QBarSet>
+#include <QtCharts/QPieSeries>
+#include <QtCharts/QPieSlice>
+#include<QString>
+#include<QPair>
+#include "abstractadapter.h"
+
+class AbstractChartAdapter : public AbstractAdapter
+{
+private:
+    QtCharts::QChart *model;
+    QtCharts::QChartView *view;
+    virtual QtCharts::QAbstractSeries* putDataInSeries(const QVector<QPair<QString,float>> &)=0;
+    void showLegend();
+    QVector<QPair<QString,float>> makeMapToVector(const QMap<QString, float> &);
+public:
+    AbstractChartAdapter();
+    virtual ~AbstractChartAdapter();
+public slots:
+    void updateAdapter(QMap<QString,float>);
+};
+
+#endif // ABSTRACTCHARTADAPTER_H
